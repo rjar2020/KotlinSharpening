d6d35fb1cdb598510daa465f219239de7a9e2341
Rolando Angelini <rjar2020@gmail.com>
2024-03-20 12:15:28 +0100

    Playing with dates and inputs

--- src/main/kotlin/basics/InputStuff.kt
+++ src/main/kotlin/basics/InputStuff.kt
@@ -1,10 +1,13 @@
 package basics
 
+import java.time.LocalDate
+import java.time.Year
+import java.util.*
 import kotlin.random.Random
 
 fun main() {
     print("Input stuff: ")
-    val userInput = readln()
+    var userInput = readln()
     println("You said what? -> $userInput")
 
     print("Give me some numbers: ")
@@ -17,4 +20,11 @@ fun main() {
     println(Random.nextInt(20, 30))
     println(Random.nextDouble(500.0))
     println(Random.nextDouble(50.0, 500.0))
+
+    print("Greetings, I'm the machine. How should I call you? -> ")
+    userInput = readln()
+    println("Nice to greet you $userInput")
+    print("$userInput could you please tell me which year were you born? -> ")
+    val birthYear = readln().toInt()
+    println("I see, so you are around ${Year.now().value - birthYear} years old.")
 }
