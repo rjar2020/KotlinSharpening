84ab265b1f5a46ffb781db24479ad6143d44cd4b
Rolando Angelini <rjar2020@gmail.com>
2024-03-20 12:01:59 +0100

    Some random stuff

--- src/main/kotlin/basics/InputStuff.kt
+++ src/main/kotlin/basics/InputStuff.kt
@@ -1,5 +1,7 @@
 package basics
 
+import kotlin.random.Random
+
 fun main() {
     print("Input stuff: ")
     val userInput = readln()
@@ -8,4 +10,11 @@ fun main() {
     print("Give me some numbers: ")
     val numberInput = readlnOrNull()?:""
     println("So that times two is -> ${numberInput.toInt() * 2}")
+
+    println("Let's get random")
+    println(Random.nextInt())
+    println(Random.nextInt(10))
+    println(Random.nextInt(20, 30))
+    println(Random.nextDouble(500.0))
+    println(Random.nextDouble(50.0, 500.0))
 }
