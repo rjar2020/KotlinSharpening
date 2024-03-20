e89f77aa983e439de8a388bd733d86f109c16b07
Rolando Angelini <rjar2020@gmail.com>
2024-03-20 12:56:12 +0100

    wrapping-up types

--- src/main/kotlin/basics/NumVars.kt
+++ src/main/kotlin/basics/NumVars.kt
@@ -2,7 +2,26 @@ package basics
 
 fun main() {
     val price = 34
-    val items = 3
+    var items = 3
     val totalPrice = price * items
     println("Total price is $totalPrice")
+
+    items = 5
+    println(items::class.java)
+
+    val numberOfPeople = 75000000000
+    println(numberOfPeople::class.java)
+
+    val dprice = 29.99
+    println(dprice::class.java)
+
+    val pi = 3.14159834768945
+    println(pi::class.java)
+
+    print("How many apples would you like to buy? -> ")
+    val apples = readln().toInt()
+    val applePrice = 1.2
+    val total = apples * applePrice
+    println("So you would pay $total € Fun fact: stored as ${total::class.java} in our system")
+    println("Btw, the price by apple is $applePrice € Fun fact: stored as ${applePrice::class.java} in our system")
 }
