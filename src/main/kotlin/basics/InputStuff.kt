e2b8153624faea45ba1e6e9b15acdd31af24d71a
Rolando Angelini <rjar2020@gmail.com>
2024-03-20 11:47:13 +0100

    Some console number input

--- src/main/kotlin/basics/InputStuff.kt
+++ src/main/kotlin/basics/InputStuff.kt
@@ -4,4 +4,8 @@ fun main() {
     print("Input stuff: ")
     val userInput = readln()
     println("You said what? -> $userInput")
+
+    print("Give me some numbers: ")
+    val numberInput = readlnOrNull()?:""
+    println("So that times two is -> ${numberInput.toInt() * 2}")
 }
