79bb6bc0afe3c281a4d0a48abd568f5d99e2328d
Rolando Angelini <rjar2020@gmail.com>
2024-04-09 09:34:03 +0200

    Playing in the void, being safe on my own

--- src/main/kotlin/basics/Nulla.kt
+++ src/main/kotlin/basics/Nulla.kt
@@ -22,4 +22,40 @@ fun main() {
     println("catName lenght is ${catName?.length?.toString()}")
     catName = "Mittens"
     println("catName lenght is ${catName?.length?.toString()}")
+
+    var theInt: Int? = 10
+    //It doesn't compile
+    //println(theInt.plus(3))
+    println(theInt?.plus(3))
+    println(theInt?.minus(3))
+    println(theInt?.times(3))
+    println(theInt?.div(3))
+    println(theInt?.rem(3))
+    println(theInt?.compareTo(3))
+    println(theInt?.compareTo(10))
+
+    var a: String? = "I like dogs"
+    //It doesn't compile
+    //println(a.substring("3,6"))
+    println(a?.substring(3,6))
+
+    var b: Double? = 3.14
+    //It doesn't compile
+    //b *= 2
+    b = b?.times(2)
+    println(b)
+
+    print("Enter your username: ")
+    var username = readLine()
+    //It doesn't compile
+    //println("userName length is ${username.length}")
+    println("userName length is ${username?.length}")
+
+    print("Enter your age in decimal: ")
+    var age = readLine()
+    //It doesn't compile
+    //age = age.toDouble()
+    age = age?.toDouble()?.times(7).toString()
+    println("Your age in dog years is $age")
+    println("Your age lenght is ${age?.length}")
 }
