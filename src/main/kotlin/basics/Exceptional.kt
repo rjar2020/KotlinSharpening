99b13a41c343d3f553a752a0343eefd6e6e63241
Rolando Angelini <rjar2020@gmail.com>
2024-04-09 11:42:39 +0200

    Exceptional wrap-up

--- src/main/kotlin/basics/Exceptional.kt
+++ src/main/kotlin/basics/Exceptional.kt
@@ -16,5 +16,22 @@ fun main() {
         println("All the computational resources involved should be released here and the program should exit gracefully")
     }
     //Just for fun
-    throw IllegalStateException("You know what? I'm done with this program. I'm out!")
+    //throw IllegalStateException("You know what? I'm done with this program. I'm out!")
+
+    print("Which product would you like to buy? ")
+    val product = readLine()
+    print("How many products would you like to buy? ")
+    val amount = readLine()
+    try {
+        println("You want to buy $amount $product")
+        println("The total price is ${amount?.toInt()?.times(9.99)}")
+    } catch (e: NumberFormatException) {
+        println("You entered: $amount, which is not a suitable amount")
+        //Don't do this in production code, it should go to your logs in the best case scenario
+        print(e.stackTraceToString())
+        println("The error message is: ${e.localizedMessage}")
+    } finally {
+        println("Thank you for using the product purchase calculator")
+        println("All the computational resources involved should be released here and the program should exit gracefully")
+    }
 }
