490a5816ba69cc68134d36125674a67e76a2ba2b
Rolando Angelini <rjar2020@gmail.com>
2024-04-09 10:57:06 +0200

    Fixing buggy code, but introducing a loudy stackTrace

--- src/main/kotlin/basics/Exceptional.kt
+++ src/main/kotlin/basics/Exceptional.kt
@@ -4,6 +4,12 @@ fun main() {
     println("Let's calculate you net worth in 5 years in terms of cash")
     print("Enter how much do you have in the bank today: ")
     val test = readln()
-    //When test is a string, the following line will throw an exception
-    println("You entered: ${test.toInt()}")
+    try {
+        println("You entered: ${test.toInt()}")
+    } catch (e: NumberFormatException) {
+        println("You entered: $test, which is not a number")
+        println("The error message is: ${e.localizedMessage}")
+        //Don't do this in production code
+        e.printStackTrace()
+    }
 }
