b93a92416bd38a63b5b1101d8cf879296f537bd0
Rolando Angelini <rjar2020@gmail.com>
2024-04-22 16:24:12 +0200

    More conditional logic, commenting some prompts

--- src/main/kotlin/flow/IfOnly.kt
+++ src/main/kotlin/flow/IfOnly.kt
@@ -1,7 +1,7 @@
 package flow
 
 fun main() {
-    println("Would you like something sweet or salty?")
+    /*println("Would you like something sweet or salty?")
     val answer = readlnOrNull()
     if (answer == "sweet") {
         println("Here is a chocolate bar for you!")
@@ -9,5 +9,24 @@ fun main() {
         println("Here are some chips for you!")
     } else {
         println("I don't have that!")
+    }
+    println("Is it night time?")
+    val night = readlnOrNull()?.toBoolean() ?: false
+    if (night) {
+        println("Good night!")
+    } else {
+        println("Good day!")
+    }*/
+
+    var clientFunds = 100
+    val price = 50
+    var clientProducts = 0
+    if (clientFunds >= price) {
+        println("You can buy this item!")
+        clientFunds -= price
+        clientProducts++
+        println("You have $clientFunds left and $clientProducts product(s)!")
+    } else {
+        println("You don't have enough money!")
     }
 }
