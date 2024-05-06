34cb8a10be6e60b2352ec0ccab5043988f861a4b
Rolando Angelini <rjar2020@gmail.com>
2024-05-06 15:11:39 +0200

    Capturing and using the value of an expresion locally in a When statement

--- src/main/kotlin/flow/ThenWhen.kt
+++ src/main/kotlin/flow/ThenWhen.kt
@@ -16,4 +16,11 @@ fun main() {
         else -> "odd"
     }
     println("The number $number is $result.")
+
+    val name = "Lucia"
+    when(val length = name.length) {
+        in 1..5 -> println("A name like $name with $length characters is a short one")
+        in 6..10 -> println("A name like $name with $length characters is a medium one")
+        else -> println("A name like $name with $length characters is a long one")
+    }
 }
