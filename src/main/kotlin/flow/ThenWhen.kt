649d78a99c90105880da076a5da8d79d59c213e3
Rolando Angelini <rjar2020@gmail.com>
2024-05-06 15:04:18 +0200

    Refactor to assign the value from a when expression

--- src/main/kotlin/flow/ThenWhen.kt
+++ src/main/kotlin/flow/ThenWhen.kt
@@ -2,20 +2,18 @@ package flow
 
 fun main() {
     val animal = "cat"
-    var action = ""
-    when(animal) {
-        "cat" -> action = "pet it"
-        "dog" -> action = "walk it"
-        "fish" -> action = "feed it"
-        else -> action = "leave it alone"
+    val action = when(animal) {
+        "cat" -> "pet it"
+        "dog" -> "walk it"
+        "fish" -> "feed it"
+        else -> "leave it alone"
     }
     println("You have a $animal. You should $action.")
 
-    var result = ""
     val number = 2345
-    when( number % 2 ) {
-        0 -> result = "even"
-        1 -> result = "odd"
+    val result  = when( number % 2 ) {
+        0 ->  "even"
+        else -> "odd"
     }
     println("The number $number is $result.")
 }
